aff0b34d4d892177e57275c93783f4b5f4fd93f7
Kristea Gamel <krgamel@gmail.com>
2019-06-22 13:07:31 -0500

    Display H2s test

--- baseball/src/components/Display.test.js
+++ baseball/src/components/Display.test.js
@@ -8,3 +8,20 @@ import Display from './Display.js';
 it('should render', () => {
     render(<Display />)
 });
+
+it('should display balls, strikes, outs, hits and inning', () => {
+    const { getByText } = render(<Display />);
+
+    const balls = getByText(/Balls:/i);
+    const strikes = getByText(/Strikes:/i);
+    const outs = getByText(/Outs:/i);
+    const hits = getByText(/Hits:/i);
+    const inning = getByText(/Inning:/i);
+
+    expect(balls).toBeInTheDocument();
+    expect(strikes).toBeInTheDocument();
+    expect(outs).toBeInTheDocument();
+    expect(hits).toBeInTheDocument();
+    expect(inning).toBeInTheDocument();
+
+});
