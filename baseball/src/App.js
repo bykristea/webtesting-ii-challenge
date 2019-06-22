a7fba0c28889faa3afa8f3a52ca3aeffcce04091
Kristea Gamel <krgamel@gmail.com>
2019-06-22 12:54:09 -0500

    added testid to buttons

--- baseball/src/App.js
+++ baseball/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import Display from './components/Display';
 function App() {
   return (
     <div className="App">
+      <Display />
       
     </div>
   );
--- baseball/src/components/Dashboard.js
+++ baseball/src/components/Dashboard.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
 import '../App.css';
 
-class Dashboard extends Component {
-
-
-
-  render() {
+function Dashboard (props) {
     return (
       <div >
-        <button>Strike</button>
-        <button>Ball</button>
-        <button>Outs</button>
-        <button>Inning</button>
+        <button data-testid="strike-btn" onClick={props.strikesHandler}>Strike</button>
+        <button data-testid="ball-btn" onClick={props.ballsHandler}>Ball</button>
+        <button data-testid="outs-btn" onClick={props.outsHandler}>Outs</button>
+        <button data-testid="hits-btn" onClick={props.hitsHandler}>Hits</button>
+        <button data-testid="inning-btn" onClick={props.inningHandler}>Inning</button>
       </div>
     );
   }
-}
+
 
 export default Dashboard;
