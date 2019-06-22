c36638615438aeec42d14605b0dcc5e27986a9dd
Kristea Gamel <krgamel@gmail.com>
2019-06-22 13:00:25 -0500

    test for button render

--- baseball/src/components/Dashboard.test.js
+++ baseball/src/components/Dashboard.test.js
@@ -9,3 +9,21 @@ import Dashboard from './Dashboard.js';
 it('should render', () => {
     render(<Dashboard />)
 });
+
+it('should display one button each saying Strike, Ball, Foul, Hit', () => {
+    const { getByTestId } = render(<Dashboard />);
+
+    const strikeBtn = getByTestId('strike-btn');
+    const ballsBtn = getByTestId('ball-btn');
+    const outsBtn = getByTestId('outs-btn');
+    const hitBtn = getByTestId('hits-btn');
+    const inningBtn = getByTestId('inning-btn');
+
+
+
+    expect(strikeBtn).toHaveTextContent(/strike/i);
+    expect(ballsBtn).toHaveTextContent(/ball/i);
+    expect(outsBtn).toHaveTextContent(/outs/i);
+    expect(hitBtn).toHaveTextContent(/hit/i);
+    expect(inningBtn).toHaveTextContent(/inning/i);
+});
