4c55812f8abcf0a56a3b3ef391fa8e680a67b24e
Kristea Gamel <krgamel@gmail.com>
2019-06-22 13:19:53 -0500

    testing strike button

--- baseball/src/components/Display.test.js
+++ baseball/src/components/Display.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/react/cleanup-after-each';
 import 'jest-dom/extend-expect';
 
 import Display from './Display.js';
+import Dashboard from './Dashboard.js';
 
 it('should render', () => {
     render(<Display />)
@@ -25,3 +26,35 @@ it('should display balls, strikes, outs, hits and inning', () => {
     expect(inning).toBeInTheDocument();
 
 });
+
+it('should set balls and strikes to 0 when hit button clicked', () => {
+    const { getByText, getByTestId } = render(<Display />);
+
+    const balls = getByText(/Balls:/i);
+        const strikes = getByText(/Strikes:/i);
+        const hitBtn = getByTestId('hits-btn');
+
+        fireEvent.click(hitBtn);
+
+        expect(balls).toHaveTextContent(/0/i);
+        expect(strikes).toHaveTextContent(/0/i);
+});
+
+it('should increase strikes by one and to zero if equals three', () => {
+    const { getByText, getByTestId } = render(<Display />);
+
+    const strikes = getByText(/Strikes:/i);
+    const strikesBtn = getByTestId('strike-btn');
+
+        fireEvent.click(strikesBtn);
+        expect(strikes).toHaveTextContent(1);
+
+        fireEvent.click(strikesBtn);
+        expect(strikes).toHaveTextContent(2);
+
+        fireEvent.click(strikesBtn);
+        expect(strikes).toHaveTextContent(3);
+
+        fireEvent.click(strikesBtn);
+        expect(strikes).toHaveTextContent(0);
+})
