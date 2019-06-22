db75e9a0cda0ff6a113ada300dfdb7999052ae3c
Kristea Gamel <krgamel@gmail.com>
2019-06-22 13:10:21 -0500

    button click test

--- baseball/src/components/Dashboard.test.js
+++ baseball/src/components/Dashboard.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/react/cleanup-after-each';
 import 'jest-dom/extend-expect';
 
@@ -24,6 +24,23 @@ it('should display one button each saying Strike, Ball, Foul, Hit', () => {
     expect(strikeBtn).toHaveTextContent(/strike/i);
     expect(ballsBtn).toHaveTextContent(/ball/i);
     expect(outsBtn).toHaveTextContent(/outs/i);
-    expect(hitBtn).toHaveTextContent(/hit/i);
+    expect(hitBtn).toHaveTextContent(/hits/i);
     expect(inningBtn).toHaveTextContent(/inning/i);
+});
+
+it('should check if buttons were clicked', () => {
+    const { getByTestId } = render(<Dashboard />);
+
+    const strikeBtn = getByTestId('strike-btn');
+    const ballsBtn = getByTestId('ball-btn');
+    const outsBtn = getByTestId('outs-btn');
+    const hitBtn = getByTestId('hits-btn');
+    const inningBtn = getByTestId('inning-btn');
+
+    fireEvent.click(strikeBtn);
+    fireEvent.click(ballsBtn);
+    fireEvent.click(outsBtn);
+    fireEvent.click(hitBtn);
+    fireEvent.click(inningBtn);
+
 });
